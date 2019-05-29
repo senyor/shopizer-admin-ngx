1f666dc013ecef2c2ed0fcbf3a6b07f7ea3bb788
IvankinaMarina <m.ivankina@vironit.co.uk>
2019-05-29 22:55:01 +0300

    added formdata for login page, added validation

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'ngx-login',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      return;
+    }
+    const formData = this.form.value;
+
+    this.authService.logon(formData.username, formData.password)
+      .subscribe(res => {
+        localStorage.setItem('token', res.token);
+        this.router.navigate([ 'pages' ]);
+      });
+  }
+
+  private createForm() {
+    this.form = this.fb.group({
+      username: [ '', [ Validators.required ] ],
+      password: [ '', Validators.required ]
+    });
   }
 
 }
