c9a87897b2778007295b995bdc4e30835eeaef5c
IvankinaMarina <m.ivankina@vironit.co.uk>
2019-06-29 15:23:30 +0300

    updated user details page

--- src/app/pages/user-management/user-details/user-details.component.ts
+++ src/app/pages/user-management/user-details/user-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {Location} from '@angular/common';
+
+import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'ngx-user-details',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
+  user;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.userService.getUser(id)
+      .subscribe(res => {
+        this.user = res;
+      })
+  }
+
+  backToList() {
+    this.location.back();
   }
 
 }
