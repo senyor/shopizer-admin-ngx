4cd4976113ca3a7a83fa31f566dad9030d062f00
IvankinaMarina <m.ivankina@vironit.co.uk>
2019-07-11 16:58:45 +0300

    added checking checkbox

--- src/app/pages/catalogue/products/products-list/available-button.component.ts
+++ src/app/pages/catalogue/products/products-list/available-button.component.ts
@@ -1,19 +1,32 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { ProductService } from '../services/product.service';
+
 @Component({
-  template: `<input type="checkbox" [checked]="value"  (click)="clicked($event) "/>`,
+  template: `<input type="checkbox" [checked]="value"  (click)="clicked() "/>`,
 })
 export class AvailableButtonComponent implements OnInit {
-  @Input() value: string | number;
+  @Input() value: boolean;
 
-  constructor() {
+  constructor(
+    private productService: ProductService
+  ) {
   }
 
   ngOnInit() {
   }
 
-  clicked(name) {
-    console.log(name);
+  clicked() {
+    this.value = !this.value;
+    const product = {
+      available: this.value,
+    };
+    console.log(product);
+    // this.productService.updateProductFromTable(event.newData.id, product)
+    //   .subscribe(res => {
+    //     console.log(res);
+    //     event.confirm.resolve(event.newData);
+    //   });
   }
 
 }
