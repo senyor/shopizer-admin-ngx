3991c7793ff72a04c75c03f5a00b4c055f09cad8
IvankinaMarina <m.ivankina@vironit.co.uk>
2019-10-29 10:28:49 +0200

    added menu with product edit subfunctionality

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -176,18 +176,12 @@ export const MENU_ITEMS: MenuItem[] = [
             link: '/pages/catalogue/products/products-list',
             hidden: false
           },
-          {
-            title: 'sideNav.manageInventory',
-            key: 'sideNav.manageInventory',
-            link: '/pages/catalogue/products/manage-inventory',
-            hidden: false
-          },
-          {
-            title: 'sideNav.productToCategory',
-            key: 'sideNav.productToCategory',
-            link: '/pages/catalogue/products/association',
-            hidden: false
-          },
+          // {
+          //   title: 'sideNav.manageInventory',
+          //   key: 'sideNav.manageInventory',
+          //   link: '/pages/catalogue/products/manage-inventory',
+          //   hidden: false
+          // },
         ],
       },
       {
