c2f3de7a951f49bf4cca781a77492fb4a6c6595c
IvankinaMarina <m.ivankina@vironit.co.uk>
2019-11-25 15:59:47 +0300

    updated product group picklist

--- src/app/pages/catalogue/products-groups/products-groups-list/products-groups-list.component.ts
+++ src/app/pages/catalogue/products-groups/products-groups-list/products-groups-list.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../products/services/product.service';
 import { ProductGroupsService } from '../services/product-groups.service';
 import { StorageService } from '../../../shared/services/storage.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'ngx-products-groups-list',
@@ -41,16 +42,35 @@ export class ProductsGroupsListComponent implements OnInit {
   }
 
   moveEvent(e, type) {
-    console.log(e.items[0].id);
+    console.log(e.items[0].id, type);
     switch (type) {
       case 'toTarget':
         this.addProductToGroup(e.items[0].id, this.selectedGroup);
         break;
       case 'toSource':
+        this.removeProductFromGroup(e.items[0].id, this.selectedGroup);
         break;
       case 'allToTarget':
+        const addArray = [];
+        e.items.forEach((el) => {
+          const req = this.addProductToGroup(el.id, this.selectedGroup);
+          addArray.push(req);
+        });
+        console.log(addArray);
+        forkJoin(addArray).subscribe(res => {
+          console.log(res);
+        });
         break;
       case 'allToSource':
+        const removeArr = [];
+        e.items.forEach((el) => {
+          const req = this.removeProductFromGroup(el.id, this.selectedGroup);
+          removeArr.push(req);
+        });
+        console.log(removeArr);
+        forkJoin(removeArr).subscribe(res => {
+          console.log(res);
+        });
         break;
     }
   }
@@ -75,6 +95,8 @@ export class ProductsGroupsListComponent implements OnInit {
     this.productGroupsService.getProductsByGroup(this.selectedGroup)
       .subscribe(res => {
         console.log(res);
+        this.selectedList = [...res.products];
+        this.availableList = this.availableList.filter(n => !this.selectedList.some(n2 => n.id === n2.id));
       });
   }
 
